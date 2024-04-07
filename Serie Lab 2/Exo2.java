6b62dc878307071e02939ca1fa77cecadce78535
yacine <a_zeraibi@estin.dz>
2024-04-07 17:29:38 +0100

    exo 2 serie lab 2

--- Serie Lab 2/Exo2.java
+++ Serie Lab 2/Exo2.java
@@ -4,7 +4,22 @@ Exercise 2: Abstraction
 current point and another point passed as an argument. Use this method to calculate and display the distance
 between the current point and itself then between the current point and point(9,3).
 */
+class PointExo2 {
+    private int x;
+    private int y;
+    public PointExo2(int x , int y){
+        this.x = x;
+        this.y = y;
+    }
+    public double calculateDistanceTo(PointExo2 otherPoint){
+        return Math.sqrt(Math.pow(this.x - otherPoint.x, 2) + Math.pow(this.y - otherPoint.y, 2));
+    }
+}
 public class Exo2 {
     public static void main(String[] args) {
+        PointExo2 p = new PointExo2(3, 4);
+        System.out.println("Distance between p and p: " + p.calculateDistanceTo(p));
+        PointExo2 p2 = new PointExo2(9, 3);
+        System.out.println("Distance between p and p2: " + p.calculateDistanceTo(p2));
     }
 }
