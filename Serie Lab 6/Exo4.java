b6d7ca1e6ab34ab29bbb94fade6fa5be106f3d77
yacine <a_zeraibi@estin.dz>
2024-05-18 20:43:00 +0100

    exo 4 - serie lab 6

--- Serie Lab 6/Exo4.java
+++ Serie Lab 6/Exo4.java
@@ -1,5 +1,27 @@
+/* Question : Write a program that reads two integers from the command line and performs division. Handle
+`NumberFormatException` if the input is not an integer, and `ArithmeticException` if there is division by zero. */
+
+import java.util.Scanner;
+
 public class Exo4 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first integer ");
+        int a = scanner.nextInt();
+        System.out.println("Enter second integer ");
+        int b = scanner.nextInt();
+        try{
+            System.out.println(a / b);
+
+        }catch(ArithmeticException e){
+            System.out.println("Division by zero is not allowed.");
+        }catch(NumberFormatException e){
+            System.out.println("Input is not an integer.");
+        }
+        catch(Exception e){
+            System.out.println("An error occurred.");
+        }
+        scanner.close();
+
     }
 }
