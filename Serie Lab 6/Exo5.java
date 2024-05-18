1a24c9e6a2101330ef07f3d738a89f229d1288bf
yacine <a_zeraibi@estin.dz>
2024-05-18 20:46:06 +0100

    exo 5 - serie lab 6

--- Serie Lab 6/Exo5.java
+++ Serie Lab 6/Exo5.java
@@ -1,5 +1,32 @@
+/* Question : Write a program that performs file operations using a `try-catch` block and includes a `finally` block to close the
+file even if an exception occurs. */
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Exo5 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("file.txt"));
+            String line = reader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
+        }catch(IOException e){
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }finally{
+            try{
+                if(reader != null){
+                    reader.close();
+                }
+            }catch(IOException e){
+                System.out.println("An error occurred while closing the file.");
+                e.printStackTrace();
+            }
+        }
+
 }
