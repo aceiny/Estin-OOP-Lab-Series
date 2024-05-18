cefcd247af85c93fd0c5b7ab789fd2282bd34e9a
yacine <a_zeraibi@estin.dz>
2024-05-18 20:30:09 +0100

    exo 2 - serie lab 6

--- Serie Lab 6/Exo2.java
+++ Serie Lab 6/Exo2.java
@@ -1,5 +1,13 @@
+/* Question : Write a program that takes two integer inputs from the command line and performs division. Use a try-catch
+block to handle `ArithmeticException` when dividing by zero and print a custom error message. */
 public class Exo2 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        try {
+            System.out.println(a / b);
+        } catch (ArithmeticException e) {
+            System.out.println("Division by zero is not allowed.");
+        }
     }
 }
